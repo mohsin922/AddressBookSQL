1c5900b362e14848cc5b69f01a1212e2bcfe92ee
peermohsin22 <peermohsin22@gmail.com>
2022-01-23 20:44:01 +0530

    Create table

--- AddressBook.sql
+++ AddressBook.sql
@@ -6,3 +6,18 @@ CREATE DATABASE AddressBookService
 Go
 use AddressBookService
 Go
+
+--UC2
+--Create table
+CREATE TABLE AddressBookTable(
+FirstName varchar(MAX) not null,
+LastName varchar(MAX) Not Null,
+Address varchar(MAX) Not Null default 'Kashmir',
+City varchar(MAX) Not Null,
+StateName varchar(MAX) Not Null,
+ZipCode int Not Null,
+Phonenum bigint Not Null,
+EmailId varchar(MAX) Not Null 
+)
+Go
+exec sp_help AddressBookTable
